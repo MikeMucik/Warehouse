d75ada027b997d631972f85b6c4556194c313e64
Michał Mucik <mucik.michal@gmail.com>
2023-12-19 21:58:58 +0100

    Add project files.

---
+++ Warehouse/Helpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse
+{
+    public class Helpers
+    {
+        public int GiveNumber()
+        {
+            SomeStructure stru = new SomeStructure();
+       
+            int number = 7;
+
+            return number;
+        }
+    }
+    public enum ItemTypes
+    {
+        Cups,
+        Forks,
+        Knifes
+    }
+
+
+    public struct SomeStructure
+    {
+        public int numberForStructure;
+        public string nameOfStructure;
+
+        public SomeStructure(int number, string name)
+        {
+            numberForStructure = number;
+            nameOfStructure = name;
+        }
+
+    }
+}
